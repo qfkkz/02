409d1058971d4bc8fe6867f14a1bfed4ee437c02
qfkkz <1947297840@qq.com>
2019-05-07 20:19:07 +0800

    1

---
+++ src/pojo/User.java
@@ -0,0 +1,42 @@
+package pojo;
+
+public class User {
+	private String account;
+	private String password;
+	private int type;
+
+	public User() {
+		super();
+	}
+
+	public User(String account, String password) {
+		super();
+		this.account = account;
+		this.password = password;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+}
